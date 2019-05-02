99822e7d4d9cd46e894d52d68bc4e3cd03999111
Apiquet <anthony.piquet2@gmail.com>
2019-05-02 15:14:52 +0200

    Detecting st pellegrino

--- opencv_cpp/opencv_cpp/opencv_cpp.cpp
+++ opencv_cpp/opencv_cpp/opencv_cpp.cpp
@@ -6,10 +6,10 @@
 using namespace std;
 using namespace cv;
 
-Mat src, src_, src_gray;
+Mat src, src_, src_gray, drawing;
 Mat dst, detected_edges, dst_threshold;
-int min_r = 0, min_g = 83, min_b = 30; 
-int max_r = 118, max_g = 135, max_b = 83;
+int min_r = 0, min_g = 98, min_b = 30; 
+int max_r = 118, max_g = 135, max_b = 75;
 int lowThreshold = 0;
 const int max_lowThreshold = 255;
 const int ratio_ = 3;
@@ -20,6 +20,10 @@ int dilation_elem = 0;
 int dilation_size = 8;
 int const max_elem = 2;
 int const max_kernel_size = 21;
+
+int thresh = 100;
+int max_thresh = 255;
+RNG rng(12345);
 
 static void CannyThreshold(int, void*)
 {
@@ -44,7 +48,7 @@ void Erosion(int, void*)
 		Size(2 * erosion_size + 1, 2 * erosion_size + 1),
 		Point(erosion_size, erosion_size));
 	erode(src_, src_, element);
-	imshow("Erosion", src_);
+	//imshow("Erosion", src_);
 }
 void Dilation(int, void*)
 {
@@ -56,9 +60,32 @@ void Dilation(int, void*)
 		Size(2 * dilation_size + 1, 2 * dilation_size + 1),
 		Point(dilation_size, dilation_size));
 	dilate(src_, src_, element);
-	imshow("Dilation", src_);
+	//imshow("Dilation", src_);
 }
+/** @function thresh_callback */
+void thresh_callback(int, void*)
+{
+	Mat canny_output;
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
 
+	/// Detect edges using canny
+	Canny(src_gray, canny_output, thresh, thresh * 2, 3);
+	/// Find contours
+	findContours(canny_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
+
+	/// Draw contours
+	drawing = Mat::zeros(dst.size(), CV_8UC3);
+	for (int i = 0; i < contours.size(); i++)
+	{
+		Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+		drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
+	}
+
+	/// Show in a window
+	//namedWindow("Contours", WINDOW_AUTOSIZE);
+	//imshow("Contours", drawing);
+}
 int main(int, char**)
 {
 	VideoCapture cap(1); // open the default camera
@@ -66,10 +93,10 @@ int main(int, char**)
 		return -1;
 
 	Mat edges;
-	namedWindow("edges", 1);
+	//namedWindow("edges", 1);
 	for (;;)
 	{
-		Mat frame;
+		Mat frame1, frame2, frame3;
 		cap >> src; // get a new frame from camera
 		//cvtColor(frame, edges, COLOR_BGR2GRAY);
 		//GaussianBlur(frame, edges, Size(7, 7), 2000, 2000);
@@ -86,17 +113,17 @@ int main(int, char**)
 		createTrackbar("max G:", "Color", &max_g, 255, colorThreshold);
 		createTrackbar("max B:", "Color", &max_b, 255, colorThreshold);
 
-		namedWindow("edges", WINDOW_AUTOSIZE);
+		//namedWindow("edges", WINDOW_AUTOSIZE);
 
 		/// Create Windows
-		namedWindow("Threshold", 1);
+		//namedWindow("Threshold", 1);
 
-		createTrackbar("Min Threshold:", "Threshold", &lowThreshold, max_lowThreshold, CannyThreshold);
+		//createTrackbar("Min Threshold:", "Threshold", &lowThreshold, max_lowThreshold, CannyThreshold);
 		
 		colorThreshold(0, 0);
 		//cvtColor(src_, src_gray, COLOR_BGR2GRAY);
-		namedWindow("Erosion Demo", WINDOW_AUTOSIZE);
-		namedWindow("Dilation Demo", WINDOW_AUTOSIZE);
+		//namedWindow("Erosion Demo", WINDOW_AUTOSIZE);
+		//namedWindow("Dilation Demo", WINDOW_AUTOSIZE);
 		moveWindow("Dilation Demo", src.cols, 0);
 		createTrackbar("Element:\n 0: Rect \n 1: Cross \n 2: Ellipse", "Erosion Demo",
 			&erosion_elem, max_elem,
@@ -112,9 +139,10 @@ int main(int, char**)
 			Dilation);
 		Erosion(0, 0);
 		Dilation(0, 0);
-		CannyThreshold(0, 0);
-
-		imshow("RESULT", src + dst);
+		//CannyThreshold(0, 0);
+		createTrackbar(" Canny thresh:", "Source", &thresh, max_thresh, thresh_callback);
+		thresh_callback(0, 0);
+		imshow("RESULT", src + drawing);
 		if (waitKey(30) >= 0) break;
 	}
 	// the camera will be deinitialized automatically in VideoCapture destructor
