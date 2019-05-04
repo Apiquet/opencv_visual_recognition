9deb4600da39b7018d00e3b8819b5f9fcac81b5f
Apiquet <anthony.piquet2@gmail.com>
2019-05-04 17:18:11 +0200

    Adding new project for ML

--- opencv_cpp/opencv_cpp/opencv_cpp.cpp
+++ opencv_cpp/opencv_cpp/opencv_cpp.cpp
@@ -8,7 +8,7 @@ using namespace cv;
 
 Mat src, src_, src_gray, drawing;
 Mat dst, detected_edges, dst_threshold;
-int min_r = 0, min_g = 98, min_b = 30; 
+int min_r = 0, min_g = 79, min_b = 30; 
 int max_r = 118, max_g = 135, max_b = 75;
 int lowThreshold = 0;
 const int max_lowThreshold = 255;
@@ -94,56 +94,59 @@ int main(int, char**)
 
 	Mat edges;
 	//namedWindow("edges", 1);
-	for (;;)
-	{
-		Mat frame1, frame2, frame3;
-		cap >> src; // get a new frame from camera
-		//cvtColor(frame, edges, COLOR_BGR2GRAY);
-		//GaussianBlur(frame, edges, Size(7, 7), 2000, 2000);
+	while (true) {
+		for (int i = 0; i<3; i++)
+		{
+			cap >> src; // get a new frame from camera
+			//cvtColor(frame, edges, COLOR_BGR2GRAY);
+			//GaussianBlur(frame, edges, Size(7, 7), 2000, 2000);
 
-		//Canny(edges, edges, 0, 20, 3);
-		dst.create(src.size(), src.type());
-		/// Create Windows
-		namedWindow("Color", 1);
+			//Canny(edges, edges, 0, 20, 3);
+			dst.create(src.size(), src.type());
+			/// Create Windows
+			namedWindow("Color", 1);
 
-		createTrackbar("min R:", "Color", &min_r, 255, colorThreshold);
-		createTrackbar("min G:", "Color", &min_g, 255, colorThreshold);
-		createTrackbar("min B:", "Color", &min_b, 255, colorThreshold);
-		createTrackbar("max R:", "Color", &max_r, 255, colorThreshold);
-		createTrackbar("max G:", "Color", &max_g, 255, colorThreshold);
-		createTrackbar("max B:", "Color", &max_b, 255, colorThreshold);
+			createTrackbar("min R:", "Color", &min_r, 255, colorThreshold);
+			createTrackbar("min G:", "Color", &min_g, 255, colorThreshold);
+			createTrackbar("min B:", "Color", &min_b, 255, colorThreshold);
+			createTrackbar("max R:", "Color", &max_r, 255, colorThreshold);
+			createTrackbar("max G:", "Color", &max_g, 255, colorThreshold);
+			createTrackbar("max B:", "Color", &max_b, 255, colorThreshold);
 
-		//namedWindow("edges", WINDOW_AUTOSIZE);
+			//namedWindow("edges", WINDOW_AUTOSIZE);
 
-		/// Create Windows
-		//namedWindow("Threshold", 1);
+			/// Create Windows
+			//namedWindow("Threshold", 1);
 
-		//createTrackbar("Min Threshold:", "Threshold", &lowThreshold, max_lowThreshold, CannyThreshold);
-		
-		colorThreshold(0, 0);
-		//cvtColor(src_, src_gray, COLOR_BGR2GRAY);
-		//namedWindow("Erosion Demo", WINDOW_AUTOSIZE);
-		//namedWindow("Dilation Demo", WINDOW_AUTOSIZE);
-		moveWindow("Dilation Demo", src.cols, 0);
-		createTrackbar("Element:\n 0: Rect \n 1: Cross \n 2: Ellipse", "Erosion Demo",
-			&erosion_elem, max_elem,
-			Erosion);
-		createTrackbar("Kernel size:\n 2n +1", "Erosion Demo",
-			&erosion_size, max_kernel_size,
-			Erosion);
-		createTrackbar("Element:\n 0: Rect \n 1: Cross \n 2: Ellipse", "Dilation Demo",
-			&dilation_elem, max_elem,
-			Dilation);
-		createTrackbar("Kernel size:\n 2n +1", "Dilation Demo",
-			&dilation_size, max_kernel_size,
-			Dilation);
-		Erosion(0, 0);
-		Dilation(0, 0);
-		//CannyThreshold(0, 0);
-		createTrackbar(" Canny thresh:", "Source", &thresh, max_thresh, thresh_callback);
-		thresh_callback(0, 0);
+			//createTrackbar("Min Threshold:", "Threshold", &lowThreshold, max_lowThreshold, CannyThreshold);
+
+			colorThreshold(0, 0);
+			//cvtColor(src_, src_gray, COLOR_BGR2GRAY);
+			//namedWindow("Erosion Demo", WINDOW_AUTOSIZE);
+			//namedWindow("Dilation Demo", WINDOW_AUTOSIZE);
+			moveWindow("Dilation Demo", src.cols, 0);
+			createTrackbar("Element:\n 0: Rect \n 1: Cross \n 2: Ellipse", "Erosion Demo",
+				&erosion_elem, max_elem,
+				Erosion);
+			createTrackbar("Kernel size:\n 2n +1", "Erosion Demo",
+				&erosion_size, max_kernel_size,
+				Erosion);
+			createTrackbar("Element:\n 0: Rect \n 1: Cross \n 2: Ellipse", "Dilation Demo",
+				&dilation_elem, max_elem,
+				Dilation);
+			createTrackbar("Kernel size:\n 2n +1", "Dilation Demo",
+				&dilation_size, max_kernel_size,
+				Dilation);
+			Erosion(0, 0);
+			Dilation(0, 0);
+			//CannyThreshold(0, 0);
+			createTrackbar(" Canny thresh:", "Source", &thresh, max_thresh, thresh_callback);
+			thresh_callback(0, 0);
+			
+			if (waitKey(30) >= 0) break;
+		}
+
 		imshow("RESULT", src + drawing);
-		if (waitKey(30) >= 0) break;
 	}
 	// the camera will be deinitialized automatically in VideoCapture destructor
 	return 0;
