6138071efbae6096784b510bfc3f2d926254f081
Apiquet <anthony.piquet2@gmail.com>
2019-05-05 01:34:16 +0200

    Filtering with LED

--- ML_image_processing/ML_image_processing/ML_image_processing.cpp
+++ ML_image_processing/ML_image_processing/ML_image_processing.cpp
@@ -1,21 +1,92 @@
-// ML_image_processing.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
-#include <iostream>
+#include<iostream>
 
-int main()
+#include<opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+Mat src, src_, src_gray, drawing;
+Mat dst, detected_edges, dst_threshold;
+int min_r = 229, min_g = 239, min_b = 0;
+int max_r = 255, max_g = 255, max_b = 229;
+int lowThreshold = 0;
+const int max_lowThreshold = 255;
+const int ratio_ = 3;
+const int kernel_size = 3;
+int erosion_elem = 0;
+int erosion_size = 2;
+int dilation_elem = 0;
+int dilation_size = 18;
+int const max_elem = 2;
+int const max_kernel_size = 21;
+
+int thresh = 100;
+int max_thresh = 255;
+RNG rng(12345);
+static void colorThreshold(int, void*)
 {
-    std::cout << "Hello World!\n"; 
+	inRange(src, Scalar(min_r, min_g, min_b), Scalar(max_r, max_g, max_b), src_gray);
+	//imshow("SRC_threshold", src_gray);
 }
+void thresh_callback(int, void*)
+{
+	Mat canny_output;
+	Canny(src_gray, canny_output, thresh, thresh * 2);
+	vector<vector<Point> > contours;
+	findContours(canny_output, contours, RETR_TREE, CHAIN_APPROX_SIMPLE);
+	vector<vector<Point> > contours_poly(contours.size());
+	vector<Rect> boundRect(contours.size());
+	vector<Point2f>centers(contours.size());
+	vector<float>radius(contours.size());
+	for (size_t i = 0; i < contours.size(); i++)
+	{
+		approxPolyDP(contours[i], contours_poly[i], 3, true);
+		boundRect[i] = boundingRect(contours_poly[i]);
+		minEnclosingCircle(contours_poly[i], centers[i], radius[i]);
+	}
+	drawing = Mat::zeros(canny_output.size(), CV_8UC3);
+	for (size_t i = 0; i < contours.size(); i++)
+	{
+		Scalar color = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
+		drawContours(drawing, contours_poly, (int)i, color);
+		rectangle(drawing, boundRect[i].tl(), boundRect[i].br(), color, 20);
+		circle(drawing, centers[i], (int)radius[i], color, 20);
+	}
+	//imshow("Contours", drawing);
+}
+int main(int, char**)
+{
+	VideoCapture cap(1); // open the default camera
+	if (!cap.isOpened())  // check if we succeeded
+		return -1;
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+	while (true) {
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+		cap >> src; // get a new frame from camera
+		//cvtColor(frame, edges, COLOR_BGR2GRAY);
+		//GaussianBlur(frame, edges, Size(7, 7), 2000, 2000);
+
+
+		namedWindow("Color", 1);
+
+		createTrackbar("min R:", "Color", &min_r, 255, colorThreshold);
+		createTrackbar("min G:", "Color", &min_g, 255, colorThreshold);
+		createTrackbar("min B:", "Color", &min_b, 255, colorThreshold);
+		createTrackbar("max R:", "Color", &max_r, 255, colorThreshold);
+		createTrackbar("max G:", "Color", &max_g, 255, colorThreshold);
+		createTrackbar("max B:", "Color", &max_b, 255, colorThreshold);
+		colorThreshold(0, 0);
+		//Define your destination image
+		//cv::Mat dstImage = cv::Mat::zeros(src.size(), src.type());
+		thresh_callback(0, 0);
+		//Now you can copy your source image to destination image with masking
+		//src.copyTo(dstImage, ~src_gray);
+		//cv::imshow("dstImage", dstImage);
+		cv::imshow("dstImage", src + drawing);
+
+		if (waitKey(30) >= 0) break;
+	}
+	// the camera will be deinitialized automatically in VideoCapture destructor
+	return 0;
+}
--- opencv_cpp/opencv_cpp/opencv_cpp.cpp
+++ opencv_cpp/opencv_cpp/opencv_cpp.cpp
@@ -8,8 +8,8 @@ using namespace cv;
 
 Mat src, src_, src_gray, drawing;
 Mat dst, detected_edges, dst_threshold;
-int min_r = 0, min_g = 79, min_b = 30; 
-int max_r = 118, max_g = 135, max_b = 75;
+int min_r = 0, min_g = 0, min_b = 0; 
+int max_r = 121, max_g = 157, max_b = 55;
 int lowThreshold = 0;
 const int max_lowThreshold = 255;
 const int ratio_ = 3;
@@ -17,14 +17,77 @@ const int kernel_size = 3;
 int erosion_elem = 0;
 int erosion_size = 2;
 int dilation_elem = 0;
-int dilation_size = 8;
+int dilation_size = 18;
 int const max_elem = 2;
 int const max_kernel_size = 21;
 
 int thresh = 100;
 int max_thresh = 255;
 RNG rng(12345);
+void detect_text(Mat rgb) {
 
+	//Mat rgb;
+	// downsample and use it for processing
+	//pyrDown(large, rgb);
+	//pyrDown(rgb, rgb);
+	Mat small;
+	cvtColor(rgb, small, COLOR_BGR2GRAY);
+	//small = rgb;
+	// morphological gradient
+	Mat grad;
+	Mat morphKernel = getStructuringElement(MORPH_ELLIPSE, Size(3, 3));
+	morphologyEx(small, grad, MORPH_GRADIENT, morphKernel);
+	// binarize
+	Mat bw;
+	threshold(grad, bw, 0.0, 255.0, THRESH_BINARY | THRESH_OTSU);
+	// connect horizontally oriented regions
+	Mat connected;
+	morphKernel = getStructuringElement(MORPH_RECT, Size(9, 1));
+	morphologyEx(bw, connected, MORPH_CLOSE, morphKernel);
+	// find contours
+	Mat mask = Mat::zeros(bw.size(), CV_8UC1);
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+	findContours(connected, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_SIMPLE, Point(0, 0));
+	// filter contours
+	for (int idx = 0; idx >= 0; idx = hierarchy[idx][0]) {
+		Rect rect = boundingRect(contours[idx]);
+		Mat maskROI(mask, rect);
+		maskROI = Scalar(0, 0, 0);
+		// fill the contour
+		drawContours(mask, contours, idx, Scalar(255, 255, 255), FILLED);
+
+		RotatedRect rrect = minAreaRect(contours[idx]);
+		double r = (double)countNonZero(maskROI) / (rrect.size.width * rrect.size.height);
+
+		Scalar color;
+		int thickness = 1;
+		// assume at least 25% of the area is filled if it contains text
+		if (r > 0.25 &&
+			(rrect.size.height > 8 && rrect.size.width > 8 && rrect.size.height < 50 && rrect.size.width < 50) // constraints on region size
+			// these two conditions alone are not very robust. better to use something 
+			//like the number of significant peaks in a horizontal projection as a third condition
+			) {
+			thickness = 2;
+			color = Scalar(0, 255, 0);
+		}
+		else
+		{
+			thickness = 1;
+			color = Scalar(0, 0, 255);
+			continue;
+		}
+
+		Point2f pts[4];
+		rrect.points(pts);
+		for (int i = 0; i < 4; i++)
+		{
+			line(rgb, Point((int)pts[i].x, (int)pts[i].y), Point((int)pts[(i + 1) % 4].x, (int)pts[(i + 1) % 4].y), color, thickness);
+		}
+	}
+
+	cv::imshow("txt", rgb);
+}
 static void CannyThreshold(int, void*)
 {
 	blur(src_gray, detected_edges, Size(3, 3));
@@ -36,7 +99,7 @@ static void colorThreshold(int, void*)
 {
 	inRange(src, Scalar(min_r, min_g, min_b), Scalar(max_r, max_g, max_b), src_);
 	src_gray = src_;
-	imshow("SRC_threshold", src_);
+	//imshow("SRC_threshold", src_);
 }
 void Erosion(int, void*)
 {
@@ -95,58 +158,63 @@ int main(int, char**)
 	Mat edges;
 	//namedWindow("edges", 1);
 	while (true) {
-		for (int i = 0; i<3; i++)
-		{
-			cap >> src; // get a new frame from camera
-			//cvtColor(frame, edges, COLOR_BGR2GRAY);
-			//GaussianBlur(frame, edges, Size(7, 7), 2000, 2000);
+		
+		cap >> src; // get a new frame from camera
+		//cvtColor(frame, edges, COLOR_BGR2GRAY);
+		//GaussianBlur(frame, edges, Size(7, 7), 2000, 2000);
 
-			//Canny(edges, edges, 0, 20, 3);
-			dst.create(src.size(), src.type());
-			/// Create Windows
-			namedWindow("Color", 1);
+		//Canny(edges, edges, 0, 20, 3);
+		dst.create(src.size(), src.type());
+		/// Create Windows
+		namedWindow("Color", 1);
 
-			createTrackbar("min R:", "Color", &min_r, 255, colorThreshold);
-			createTrackbar("min G:", "Color", &min_g, 255, colorThreshold);
-			createTrackbar("min B:", "Color", &min_b, 255, colorThreshold);
-			createTrackbar("max R:", "Color", &max_r, 255, colorThreshold);
-			createTrackbar("max G:", "Color", &max_g, 255, colorThreshold);
-			createTrackbar("max B:", "Color", &max_b, 255, colorThreshold);
+		createTrackbar("min R:", "Color", &min_r, 255, colorThreshold);
+		createTrackbar("min G:", "Color", &min_g, 255, colorThreshold);
+		createTrackbar("min B:", "Color", &min_b, 255, colorThreshold);
+		createTrackbar("max R:", "Color", &max_r, 255, colorThreshold);
+		createTrackbar("max G:", "Color", &max_g, 255, colorThreshold);
+		createTrackbar("max B:", "Color", &max_b, 255, colorThreshold);
 
-			//namedWindow("edges", WINDOW_AUTOSIZE);
+		//namedWindow("edges", WINDOW_AUTOSIZE);
 
-			/// Create Windows
-			//namedWindow("Threshold", 1);
+		/// Create Windows
+		//namedWindow("Threshold", 1);
 
-			//createTrackbar("Min Threshold:", "Threshold", &lowThreshold, max_lowThreshold, CannyThreshold);
+		//createTrackbar("Min Threshold:", "Threshold", &lowThreshold, max_lowThreshold, CannyThreshold);
 
-			colorThreshold(0, 0);
-			//cvtColor(src_, src_gray, COLOR_BGR2GRAY);
-			//namedWindow("Erosion Demo", WINDOW_AUTOSIZE);
-			//namedWindow("Dilation Demo", WINDOW_AUTOSIZE);
-			moveWindow("Dilation Demo", src.cols, 0);
-			createTrackbar("Element:\n 0: Rect \n 1: Cross \n 2: Ellipse", "Erosion Demo",
-				&erosion_elem, max_elem,
-				Erosion);
-			createTrackbar("Kernel size:\n 2n +1", "Erosion Demo",
-				&erosion_size, max_kernel_size,
-				Erosion);
-			createTrackbar("Element:\n 0: Rect \n 1: Cross \n 2: Ellipse", "Dilation Demo",
-				&dilation_elem, max_elem,
-				Dilation);
-			createTrackbar("Kernel size:\n 2n +1", "Dilation Demo",
-				&dilation_size, max_kernel_size,
-				Dilation);
-			Erosion(0, 0);
-			Dilation(0, 0);
-			//CannyThreshold(0, 0);
-			createTrackbar(" Canny thresh:", "Source", &thresh, max_thresh, thresh_callback);
-			thresh_callback(0, 0);
+		colorThreshold(0, 0);
+		//cvtColor(src_, src_gray, COLOR_BGR2GRAY);
+		namedWindow("Erosion Demo", WINDOW_AUTOSIZE);
+		namedWindow("Dilation Demo", WINDOW_AUTOSIZE);
+		moveWindow("Dilation Demo", src.cols, 0);
+		createTrackbar("Element:\n 0: Rect \n 1: Cross \n 2: Ellipse", "Erosion Demo",
+			&erosion_elem, max_elem,
+			Erosion);
+		createTrackbar("Kernel size:\n 2n +1", "Erosion Demo",
+			&erosion_size, max_kernel_size,
+			Erosion);
+		createTrackbar("Element:\n 0: Rect \n 1: Cross \n 2: Ellipse", "Dilation Demo",
+			&dilation_elem, max_elem,
+			Dilation);
+		createTrackbar("Kernel size:\n 2n +1", "Dilation Demo",
+			&dilation_size, max_kernel_size,
+			Dilation);
+		Erosion(0, 0);
+		Dilation(0, 0);
+		//CannyThreshold(0, 0);
+		//createTrackbar(" Canny thresh:", "Source", &thresh, max_thresh, thresh_callback);
+		//thresh_callback(0, 0);
 			
-			if (waitKey(30) >= 0) break;
-		}
 
-		imshow("RESULT", src + drawing);
+		//Define your destination image
+		cv::Mat dstImage = cv::Mat::zeros(src.size(), src.type());
+		//Now you can copy your source image to destination image with masking
+		src.copyTo(dstImage, src_);
+		cv::imshow("dstImage", src);
+
+		//detect_text(dstImage);
+
+		if (waitKey(30) >= 0) break;
 	}
 	// the camera will be deinitialized automatically in VideoCapture destructor
 	return 0;
